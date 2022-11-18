d125bb2d4ccf3c09df68c6f8caa32260fdeab13b
TruEnot <truenot@vivaldi.net>
2022-11-19 01:25:33 +0300

    Начало разработки клиента

---
+++ client/MainWindow.py
@@ -0,0 +1,11 @@
+import tkinter
+
+
+class MainWindow(tkinter.Tk):
+    def __init__(self, title):
+        super().__init__()
+        self.title("Title")
+        self.__initUi()
+
+    def __initUi(self):
+        pass
---
+++ client/client.py
@@ -0,0 +1,7 @@
+from MainWindow import MainWindow
+
+
+if __name__=='__main__':
+    root = MainWindow('Base')
+    root.geometry('800x500')
+    root.mainloop()
