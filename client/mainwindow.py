72a122631926290ab5ffef10d0ebd6dc600f6329
Navnica <truenot@vivaldi.net>
2022-12-05 02:35:44 +0300

    этот коммит будет удален позже

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -14,37 +14,13 @@ class MainWindow(QMainWindow):
     def __initUi(self) -> None:
         self.central_widget = QWidget()
         self.main_v_layout = QVBoxLayout()
-        self.login_register_button_v_layout = QVBoxLayout()
-        self.exit_button_v_layout = QVBoxLayout()
-        self.login_button = QPushButton()
-        self.register_button = QPushButton()
-        self.exit_button = QPushButton()
 
     def __settingUi(self) -> None:
-        self.setFixedSize(300, 400)
+        self.resize(930, 615)
         self.setWindowTitle('Touragency Client')
 
         self.setCentralWidget(self.central_widget)
         self.central_widget.setLayout(self.main_v_layout)
-        self.main_v_layout.addLayout(self.login_register_button_v_layout)
-        self.main_v_layout.addLayout(self.exit_button_v_layout)
-
-        self.login_register_button_v_layout.addWidget(self.login_button)
-        self.login_register_button_v_layout.addWidget(self.register_button)
-        self.exit_button_v_layout.addWidget(self.exit_button)
-
-        self.login_register_button_v_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.exit_button_v_layout.setAlignment(Qt.AlignmentFlag.AlignRight)
-
-        self.login_button.setText('Login')
-        self.register_button.setText('Register')
-        self.exit_button.setText('Exit')
-
-        self.login_button.setMinimumWidth(200)
-
-        self.login_button.clicked.connect(self.on_login_button_clicked)
-        self.register_button.clicked.connect(self.on_register_button_clicked)
-        self.exit_button.clicked.connect(self.on_exit_button_clicked)
 
     def show_message(self, text: str, error: bool = False, parent=None) -> None:
         messagebox = QMessageBox(self if not parent else parent)
@@ -54,20 +30,46 @@ class MainWindow(QMainWindow):
         messagebox.setIcon(QMessageBox.Icon.Critical if error else QMessageBox.Icon.Information)
         messagebox.show()
 
-    def on_login_button_clicked(self) -> None:
-        self.open_login_dialog()
-
-    def on_register_button_clicked(self) -> None:
-        self.open_register_dialog()
-
-    def on_exit_button_clicked(self) -> None:
-        self.exit()
-
-    def open_login_dialog(self) -> None:
-        LoginWindow(self)
-
-    def open_register_dialog(self):
-        RegisterWindow(self)
-
     def exit(self) -> None:
         self.close()
+
+    class PageList(QWidget):
+        def __init__(self):
+            super().__init__()
+
+    class AuthorizationWidget(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.__initUi()
+            self.__settingUi()
+
+        def __initUi(self):
+            self.main_layout = QVBoxLayout()
+
+            self.button_container = QWidget(self)
+
+            self.login_button = QPushButton()
+            self.register_button = QPushButton()
+
+        def __settingUi(self):
+            self.setLayout(self.main_layout)
+
+            # Set texts for elements
+            self.login_button.setText('Login')
+            self.register_button.setText('Register')
+
+            # Appending widgets
+            self.main_layout.addWidget(self.login_button)
+            self.main_layout.addWidget(self.register_button)
+
+            # Buttons connect
+            self.login_button.clicked.connect(self.on_press_login)
+            self.register_button.clicked.connect(self.on_press_register)
+
+        def on_press_login(self):
+            LoginWindow(self.parent())
+
+        def on_press_register(self):
+            RegisterWindow(self.parent())
+
+    
